5870d7c54e951696d1f43cb4eb2dd78d3b5be3d4
Emmanuel <emmanuelogheneovo17@gmail.com>
2025-01-25 16:52:21 +0100

    r2aproject

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Events from "./components/Eventpage";
 import About from "./components/about";
 import Resources from "./components/Resources";
 import Contact from "./components/Contact";
-import Store from "./components/store";
+
 // import Donate from "./components/donate";
 import DonatePage from "./components/donate";
 import Footer from "./components/layout/Footer";
@@ -18,7 +18,7 @@ const App = () => {
           { name: "Event", path: "/event" },
           { name: "About us", path: "/about" },
           { name: "Resources", path: "/resources" },
-          { name: "Store", path: "/store" },
+
           { name: "Contact", path: "/contact" },
         ]}
         logo="path-to-logo.png"
@@ -30,7 +30,7 @@ const App = () => {
         <Route path="/event" element={<Events />} />
         <Route path="/about" element={<About />} />
         <Route path="/resources" element={<Resources />} />
-        <Route path="/store" element={<Store />} />
+
         <Route path="/contact" element={<Contact />} />
         <Route path="/donate" element={<DonatePage />} />
       </Routes>
